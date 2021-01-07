11fc67a0ac71f1cb7031a836fe16be2b28f8960a
JimmyEllercamp <james_ellercamp@hotmail.com>
2021-01-07 17:29:32 +1100

    strings for wealth and enemy filled in and connected, up to Love

--- script2.js
+++ script2.js
@@ -4,17 +4,17 @@ console.log('The fortune teller awaits. Continue if you want to learn about your
 
 const randomNumber = Math.floor(Math.random() * 5);
 
-const wealth1 = '';
-const wealth2 = '';
-const wealth3 = '';
-const wealth4 = '';
-const wealth5 = '';
+const wealth1 = 'Corn is selling well this season. You should plant some crops in your neighbours front yard, they never use it anyway.';
+const wealth2 = 'The housing market will remain out of reach for anyone who isn\'t a boomer, and every boomer will tell you "Now is the time to buy a house, this market right now, Nance and I are always keeping our eye out. Mark my words, now is the time.';
+const wealth3 = 'The stock market will continue doing things that none of us really understand anyway, but your older friends will still pretend they understand the \'fluctuations\'.';
+const wealth4 = 'You will have trouble finding your wallet, so try not let it out of your sight.';
+const wealth5 = 'A wealthy asian businessman will offer money for personal modelling photos, and it would be quite a lucrative business but you will slightly hate yourself for doing it.';
 
-const enemy1 = '';
-const enemy2 = '';
-const enemy3 = '';
-const enemy4 = '';
-const enemy5 = '';
+const enemy1 = 'Smartwatches are plotting a hostile takeover of the world, stay away from the new iOS update.';
+const enemy2 = 'There is a ninja who has been following you for weeks now. Someone you don\'t even know blamed you for sleeping with his sister. Stay in the light when walking around at night, and steer clear of any alleys, we all know Spiderman is a fictional character, and he ain\'t coming to save you.';
+const enemy3 = 'Elon Musk has been spying on you for years.';
+const enemy4 = 'You remember that one kid in your grade 2 class who kinda smelled but always gave you weird looks? He still thinks about you with disdain.';
+const enemy5 = 'Never insult a monkey weilding a sword. They never forgive';
 
 const love1 = '';
 const love2 = '';
@@ -48,6 +48,6 @@ const weekArray = [week1, week2, week3, week4, week5];
 const yearArray = [year1, year2, year3, year4, year5];
 
 const generateFortune = () => {
-    return `Wealth: ${wealthArray[randomNumber]} \n Your foes: ${enemyArray[randomNumber]} \n Your love life: ${loveArray[randomNumber]}\nYour day: ${dayArray[randomNumber]}\nYour week: ${weekArray[randomNumber]}\nYour year: ${yearArray[randomNumber]}`;
-    
+    return `I have seen your future, In my lovely shiny little ball, that you are not allowed to touch, and this is what it holds: \nWealth: ${wealthArray[randomNumber]} \nYour foes: ${enemyArray[randomNumber]} \nYour love life: ${loveArray[randomNumber]}\nYour day: ${dayArray[randomNumber]}\nYour week: ${weekArray[randomNumber]}\nYour year: ${yearArray[randomNumber]}\n\n`;
+
 }
