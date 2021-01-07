0e185dce02c1153a416756f44f6571e068f9f1f7
JimmyEllercamp <james_ellercamp@hotmail.com>
2021-01-07 14:59:04 +1100

    First commit, three user inputs defined, generateStory not defined

---
+++ script.js
@@ -0,0 +1,14 @@
+// Mixed Messages 
+
+console.log('Hello, and welcome to Mixed Messages!\nHave you ever looked a child in the face, and they have asked you to tell them a story, only for you mind to go totally blank because that is a TOTALLY unreasonable request to ask of an adult, and you are in no way prepared?\nWell, here is your solution. The Children\'s Story Random Prompt Generator!');
+
+let userInput1 = '';
+let userInput2 = '';
+let userInput3 = '';
+const animal = ['Elephant', 'Owl', 'Pigeon', 'Rabbit', 'T-Rex', 'Snake', 'Badger', 'Dolphin', 'Chimp'];
+const person = ['Child', 'Priest', 'Spy', 'Policeman', 'Postman', 'Pizza Delivery Guy', 'Politician', 'Knight', 'Princess', 'Wizard'];
+const setting = ['Forest', 'Shopping Centre', 'Abandoned Building', 'Farm', 'Treehouse', 'Tattoo Parlour', 'Sex Shop', 'Bakery', 'Theme Park', 'School'];
+
+const generateStory = (userInput1, userInput2, userInput3) => {
+    
+}
